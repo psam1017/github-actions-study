f39044b178f42dba6dcc3559b9aca8d5bca6a70f
psam1017 <psam1017@naver.com>
2024-09-15 21:32:23 +0900

    chore: v0-run.yml created. works when pushed onto v0 branch only

---
+++ src/main/java/psam1017/study/github_actions/GithubActionsApplication.java
@@ -0,0 +1,13 @@
+package psam1017.study.github_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/psam1017/study/github_actions/GithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package psam1017.study.github_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
